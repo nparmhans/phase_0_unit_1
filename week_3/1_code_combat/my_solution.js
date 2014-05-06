3267a76738b742ca1383e9be5b46019fb3ddf269
Stephen Estrada <stcrestrada@gmail.com>
2014-05-06 01:55:27 -0400

    adding pseudocode and solutions

--- week_3/1_code_combat/my_solution.js
+++ week_3/1_code_combat/my_solution.js
@@ -4,8 +4,56 @@
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
  
+// Rescue Mission:
+// move down x2
+// move right
+// move up x2
+// move right x2
+// move down
+// Attack!
+//
+// Solution:
+// this.moveDown();
+// this.moveDown();
+// this.moveRight();
+// this.moveUp();
+// this.moveUp();
+// this.moveRight();
+// this.moveRight();
+// this.moveDown();
+// this.attackNearbyEnemy();
  
- 
+// Grab the Mushroom:
+// move up
+// move right
+// move left
+// move up
+// Attack!
+
+// Solution:
+// this.moveUp();
+// this.moveRight();
+// this.moveLeft();
+// this.moveUp();
+// this.attackNearbyEnemy();
+
+//  Drink Me:
+//  Attack!
+//  move right
+//  move down
+//  move up
+//  move right
+//  Attack!
+
+//Solution:
+// this.attackNearbyEnemy();
+// this.moveRight();
+// this.moveDown();
+// this.moveUp();
+// this.moveRight();
+// this.attackNearbyEnemy();
+
+//
  
  
  
