d0f4142b9cd988092aab75c0c99da70d3721395e
Stephen Estrada <stcrestrada@gmail.com>
2014-05-08 15:09:31 -0400

    Completed solo challenge

--- week_3/4_manipulating_js_objects_solo_challenge/my_solution.js
+++ week_3/4_manipulating_js_objects_solo_challenge/my_solution.js
@@ -45,28 +45,28 @@ var terah = {
 
 // __________________________________________
 // Write your code below.
-var adam = {};
+// 1. var adam = {};
 
-var adam = {name: "Adam"};
+// 2. var adam = {name: "Adam"};
 
-terah = {spouse: adam};
+// 3. terah = {spouse: adam};
 
-terah.weight = 125;
+// 4. terah.weight = 125;
 
-delete terah.eyeColor;
+// 5. delete terah.eyeColor;
 
-adam.spouse = terah;
+// 6. adam.spouse = terah;
 
-terah.children = {};
+// 7. terah.children = {};
 
-terah.children.carson = {name: "Carson"};
+// 8. terah.children.carson = {name: "Carson"};
 
-terah.children.carter = {name: "Carter"};
+// 9. terah.children.carter = {name: "Carter"};
 
-terah.children.colton = {name:"Colton"};
+// 10. terah.children.colton = {name:"Colton"};
 
-adam.children = {};
-adam.children = terah.children
+// 11. adam.children = {};
+// adam.children = terah.children
 
 
 
@@ -80,12 +80,21 @@ adam.children = terah.children
 
 // __________________________________________
 // Reflection: Use the reflection guidelines
-// 
-// 
-// 
-// 
-// 
-// 
+// * Today's solo challenge started off very smooth; however, once I started on question 8 to 11 I began to struggle a little 
+// bit have to implement new syntax so that my code would work in the terminal. 
+
+// * What made solo questions 8 - 11 the most difficult was trying to add multiple properties and values to a property
+// that had already been define. What helps was the object literal notation notes that DBC provided in challenge 3. Also I 
+// referred back to code combat when I hovered over the keyword 'this' and noticed that it contained multiple properties and
+// values. 
+
+// * I understood the concepts of what I am doing. What makes it easier is going through the challenge three links so that
+// you're able to properly learn JavaScript. I spent about 30 minuters on questions 11. Which isn't the best time management
+// but I will know better for next time. 
+
+// * Today I learned Object literal notation at a much higher level than I had previously known as far as utilizing objects and
+// properties with values. Before, I went over object literal notation, but now I am actually putting it to good use which is 
+// definitely something I need to take into consideration while learning because it clearly works for me. 
 
 
 // __________________________________________
