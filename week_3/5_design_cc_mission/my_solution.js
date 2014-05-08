c0eb793bd83b84fddbfd10f3c694acf06bffcd71
Stephen Estrada <stcrestrada@gmail.com>
2014-05-08 10:49:10 -0400

    compelted reflection questions

--- week_3/5_design_cc_mission/my_solution.js
+++ week_3/5_design_cc_mission/my_solution.js
@@ -70,11 +70,55 @@
 
 
 // Reflection
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
+// When designing a mission challenege I worked with Avi Fox-rosen. Before we got into any detail on how we wanted to assess the challenge we went over
+// the essentials first which was checking in with one another and asking how everything is going as far as in our lives and in DBC. Next Avi and I 
+// brainstormed some solid ideas and his mission to want to have a character go through a labyrinth design to get a diaper then find the baby in the maze
+// and put the diaper on a baby is what really stuck out for both of us. We aimed to make this assignment as fun for us as possible. Here at DBC, while the
+// work-load can be overwhelming at times there is always time for enjoyment. Since Avi had a good idea on what we were building he started off as the navigator, 
+// from here we constructed a wireframe for how we wanted the labyrinth to appear and what the goals of our objects and properties were. After which we began
+// our mission design, quickly moving into the Pseudocode. Half way through once we had a solid understanding of the basics we switched rolls and Avi began
+// the driver while I started to navigate. Avi and I worked through the assignment strong. Once again it was a lot of fun working with Avi. 
+
+// * Our strategy was reading over the directions together and trying to get a full understanding for what we were dealing with. Problems we faced were trying
+// to figure out if we really needed to have JavaScript 4 on our computers and if we needed to utilize it. We never figured it out but we did compelte the 
+// assignment. 
+
+// * We asked ourselves what objects are we using in our mission design? What properties will the objects possess? What is 'this' referring to in the directions?
+// Are we hovering over the combat code 'this'? We used code combat to hover over 'this' when we discovered that it possessed multiple properties and values. 
+
+// * I don't know that Avi and I struggled too much with any concepts other than trying to distinguish how to utilize the objects and thier properties. 
+
+// * I have been struggling on question 7 of the solo challenge and trying to relay to myself how to answer this question; however, last night while 
+// working with Avi, after we learned to hover over 'this', I got a better understanding that it was to create multiple properties for values, and I'm now
+// going to try to utilize that understanding today. 
+
+// * As long as I'm working hard and focusing I am fully confident with my learning competencies. Ocassionally, as anyone else does, I will struggle with some
+// concepts or questions, but its just another feat for me :). 
+
+// * The challenge itself was made enjoyable because we were able to work with one another, while also brainstorming wild ideas on what we wanted to display to
+// users when they read over our code. 
+
+// * Nothing was tedious. I think because JavaScript is new to this DBC class or at least most of us, finding something tedious would be fairly difficult
+// as of now. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
