d23d2965ee836472339e374450b2bfc5a946b007
Stephen Estrada <stcrestrada@gmail.com>
2014-05-08 20:10:03 -0400

    Completed GPS Solution with Partner

--- week_3/9_gps1_2/our_solution.js
+++ week_3/9_gps1_2/our_solution.js
@@ -1,24 +1,27 @@
 // GPS 1.2 - JavaScript
 
 // YOUR FULL NAMES:
-//  1.
-//  2.
+//  1.Stephen Estrada
+//  2. Ernie Salazar
 
 
-// 1. "YOU SIGNED... WHO?!"
+// 0. "YOU SIGNED... WHO?!"
+function Client(name, age, quote) {
+  this.name = name;
+  this.age = age;
+  this.quote = quote;
+}
 
 
+var will = new Client("Will Smith", 45, "I like candy");
 
+// 1. "Here they Come!"
 
-// 2. "Here they Come!"
+var adam = new Client("Adam Sandler", 45, "That's your home! Are you to good for your home?!");
+var kristen = new Client("Kristen Bell", 33, "Do you wanna to build snowman?");
+var jim = new Client("Jim Carrey", 52, "...so you're telling me there's a chance? YEAH!");
 
-
-
-
-
-// 3. "TIME IS MONEY!"
-
-//YOUR CODE HERE!
+// 2. "TIME IS MONEY!"
 
 var shooterMcGavin = new Client("Shooter McGavin", 48, "Just stay out of my way... or you'll pay. Listen to what I say.");
 shooterMcGavin.constructor === Client;
@@ -26,15 +29,24 @@ shooterMcGavin.age === 48;
 shooterMcGavin.quote === "Just stay out of my way... or you'll pay. Listen to what I say.";
 
 
+//YOUR CODE HERE!
 
 
+// 3. "SHOW 'EM OFF!"
 
-// 4. "SHOW 'EM OFF!"
+var clients = [will, adam, kristen, jim, shooterMcGavin];
+console.log(clients[1]);
+console.log(clients);
 
+// 4. "BUT WAIT THERE'S MORE"
 
+will.showQuote = function() {
+  console.log(this.quote);
+}
 
 
 // ** BONUS **
 
 
 //  Your Reflection:
+
